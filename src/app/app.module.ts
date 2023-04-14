1b27ef28cde2b357dd4bac830d271ac65beae685
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-04-13 19:09:57 -0500

    Added the modules needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatStepperModule } from '@angular/material/stepper';
+import { SwiperModule } from 'swiper/angular';
+
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
@@ -8,6 +41,23 @@ import { ProductsPageComponent } from './pages/products-page/products-page.compo
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { PagenotfoundPageComponent } from './pages/pagenotfound-page/pagenotfound-page.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductdetailComponent } from './components/productdetail/productdetail.component';
+import { OrderPageComponent } from './pages/order-page/order-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { environment } from 'src/environment';
+import { NavComponent } from './components/nav/nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AdminProductsComponent } from './components/admin-products/admin-products.component';
+import { ProductsCreateComponent } from './components/products-create/products-create.component';
+import { ProductsEditComponent } from './components/products-edit/products-edit.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +66,54 @@ import { PagenotfoundPageComponent } from './pages/pagenotfound-page/pagenotfoun
     ProductsPageComponent,
     LoginPageComponent,
     AdminPageComponent,
-    PagenotfoundPageComponent
+    PagenotfoundPageComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    ProductComponent,
+    ProductdetailComponent,
+    OrderPageComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavComponent,
+    DashboardComponent,
+    AdminProductsComponent,
+    ProductsCreateComponent,
+    ProductsEditComponent,
+    CategoryComponent,
+    ProductsListComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatSortModule,
+    LayoutModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    SwiperModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
