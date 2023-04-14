3b705e7387240c25e9a39dd947ee1e59d76946c7
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-04-13 20:05:22 -0500

    Created the login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/services/auth.service';
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  form: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]]})
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.buildForm();
+  }
+
+  ngOnInit() {
+  }
+
+  login(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const value = this.form.value;
+      this.authService.login(value.email, value.password)
+      .then(() => {
+        this.router.navigate(['/admin']);
+      })
+      .catch(() => {
+        alert('no es valido');
+      });
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+
+}
