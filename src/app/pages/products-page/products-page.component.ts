a9cedb83fb695807032370671017ca990d7d9f29
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-04-13 19:57:46 -0500

    Created the page for the products with components

--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.scss']
 })
-export class ProductsPageComponent {
+export class ProductsPageComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(
+    private productsService: ProductsService
+  ) { }
+
+  ngOnInit() {
+    this.fetchProducts();
+  }
+
+
+  fetchProducts() {
+    this.productsService.getAllProducts()
+    .subscribe(products => {
+      this.products = products;
+    });
+  }
 
 }
