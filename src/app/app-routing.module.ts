da492b56ef0ae9b9e469ec98c4b073cff9427eaf
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-04-13 19:08:50 -0500

    Added the routing part with canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,40 +2,89 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { PagenotfoundPageComponent } from './pages/pagenotfound-page/pagenotfound-page.component';
-
+import { LayoutComponent } from './pages/layout/layout.component';
+import { ProductdetailComponent } from './components/productdetail/productdetail.component';
+import { OrderPageComponent } from './pages/order-page/order-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './auth.guard';
+import { NavComponent } from './components/nav/nav.component';
+import { AdminProductsComponent } from './components/admin-products/admin-products.component';
+import { ProductsCreateComponent } from './components/products-create/products-create.component';
+import { ProductsEditComponent } from './components/products-edit/products-edit.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CategoryComponent } from './components/category/category.component';
 
 
 const routes: Routes = [
-
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'home'
-  },
-  {
-    path: 'home',
-    component: WelcomePageComponent
-  },
-  {
-    path: 'products',
-    component: ProductsPageComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: WelcomePageComponent
+      },
+      {
+        path: 'products',
+        component: ProductsPageComponent
+      },
+      {
+        path: 'products/category/:category',
+        component: CategoryComponent
+      },
+      {
+        path: 'products/:id',
+        component: ProductdetailComponent
+      },
+      {
+        path: 'order',
+        component: OrderPageComponent
+      },
+
+    ]
   },
   {
     path: 'login',
-    component: LoginPageComponent
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   },
   {
     path: 'admin',
-    component: AdminPageComponent
+    canActivate: [AuthGuard],
+    component: NavComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: 'products',
+        component: AdminProductsComponent
+      },
+      {
+        path: 'products/create',
+        component: ProductsCreateComponent
+      },
+      {
+        path: 'products/edit/:id',
+        component: ProductsEditComponent
+      },
+    ]
   },
   {
-    path: '**', pathMatch: 'full',
+    path: '**',
     component: PagenotfoundPageComponent
   }
-
 ];
 
 @NgModule({
