1a39c3710c6b7652e4dc9fc48b879d6d624ae936
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-04-13 19:07:48 -0500

    Created the environment file and added images

---
+++ src/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  // production: false,
+  url_api: 'http://localhost:8080',
+   firebase: {
+     apiKey: 'AIzaSyCgZgfbQ0eDT9ziQFN1gZ-noHThcwhmesM',
+     authDomain: 'everythinginarts.firebaseapp.com',
+     //databaseURL: 'https://platzi-store-forms.firebaseio.com',
+     projectId: 'everythinginarts',
+     storageBucket: 'everythinginarts.appspot.com',
+     messagingSenderId: '887376171256',
+     appId: '1:887376171256:web:1b24508b2e3d6c7f9a2b0d'
+   }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
