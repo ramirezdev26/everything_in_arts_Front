5a7f82ec29732569e24b51a8cba77a40c7a7fed9
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-04-13 19:31:02 -0500

    Created banner for home page

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent {
+
+  images: string[] = [
+    'assets/images/banner-1.jpg',
+    'assets/images/banner-2.jpg',
+    'assets/images/banner-3.jpg',
+  ];
+
+}
